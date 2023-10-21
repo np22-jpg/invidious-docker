c20e4f1af2dcd81d240a0815e91a72d67266c57b
Nolan Poe <npgo22@gmail.com>
2023-10-20 19:45:50 -0700

    add profiled patchsets

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+type=$1
+
 set -euo pipefail
 
-patches=$(ls patches/*.patch)
+if [[ -z "$type" ]]; then
+    echo "Error: patchset type required"
+    exit 1
+fi
 
-# Get the actual version history for invidious
-rm invidious/.git 
-cp .git/modules/invidious/ invidious/.git -r
+if [[ "$type" == "vanilla" ]]; then
+    patches=""
+fi
+
+if [[ "$type" == "highload"  ]]; then
+    rm patches/010-use-redis-for-video-cache.patch
+    patches=$(ls patches/*.patch)
+fi
+
+if [[ "$type" == "redis" ]]; then
+    patches=$(ls patches/*.patch)
+fi
+
 
 for patch in $patches; do
     echo "Applying patch: $patch"
--- scripts/get-patches.sh
+++ scripts/get-patches.sh
@@ -13,9 +13,9 @@ if [ "$(ls -A custom-patches)" ]; then
     cp custom-patches/*.patch patches/
 fi
 
+
+# Always remove patches specfic to yewtu.be
 rm patches/003-proxy-csp.patch
 rm patches/004-donate-page.patch
-rm patches/009-restrict-playback.patch
-rm patches/010-use-redis-for-video-cache.patch
 rm patches/020-use-separate-domain-for-webhook-requests.patch
 rm patches/021-message-registration.patch
