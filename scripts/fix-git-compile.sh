dc8ff3af8069e7311e58aa773a222644c89682a9
Nolan Poe <npgo22@gmail.com>
2023-10-20 19:15:48 -0700

    .git repo check for invidious submodule

--- scripts/fix-git-compile.sh
+++ scripts/fix-git-compile.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 # Get the actual version history for invidious
-rm invidious/.git 
-cp .git/modules/invidious/ invidious/.git -r
+# If invidious/.git is a file, it's a git submodule
+# If it's a directory, it's a git repo
+if [[ -f invidious/.git ]]; then
+    rm invidious/.git
+    cp .git/modules/invidious/ invidious/.git -r
+fi
