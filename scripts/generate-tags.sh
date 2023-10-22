f0d4011e33d4135cb4296e9628f6404393896711
Nolan Poe <npgo22@gmail.com>
2023-10-21 17:29:39 -0700

    Move off crystal image, move tag generation to bash script

---
+++ scripts/generate-tags.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+distro=$1
+patchset=$2
+arch=$3
+
+TIMESTAMP="$(date +%Y%m%d)"
+
+PRE_BUILD_TAGS=()
+
+# Set arch tags
+if [ "$arch" == "linux/amd64" ]; then
+  arch_tag="amd64"
+elif [ "$arch" == "linux/arm64" ]; then
+  arch_tag="arm64"
+elif [ "$arch" == "linux/arm/v7" ]; then
+  arch_tag="armv7"
+elif [ "$arch" == "linux/ppc64le" ]; then
+  arch_tag="ppc64le"
+else
+  echo "Unknown arch: $arch"
+  exit 1
+fi
+
+# If it's an alpine build, create an array with "latest" and "alpine"
+# Otherwise, set PRE_BUILD_TAGS to distro
+if [ "$distro" == "alpine" ]; then
+  PRE_BUILD_TAGS+=("latest")
+fi
+PRE_BUILD_TAGS+=("${distro}")
+
+# Append patchset type to each entry in PRE_BUILD_TAGS
+for TAG in "${PRE_BUILD_TAGS[@]}"; do
+  PRE_BUILD_TAGS=("${TAG}-${patchset}")
+done
+
+if [ "$patchset" == "vanilla" ]; then
+  PRE_BUILD_TAGS+=("${TAG}")
+fi
+
+if [ "$arch_tag" == "amd64" ]; then
+  BUILD_TAGS=("${PRE_BUILD_TAGS[@]}")
+  for TAG in "${BUILD_TAGS[@]}"; do
+    BUILD_TAGS+=("${TAG}-${arch_tag}")
+  done
+else
+  for TAG in "${PRE_BUILD_TAGS[@]}"; do
+    BUILD_TAGS+=("${TAG}-${arch_tag}")
+  done
+fi
+
+# Append matching timestamp tags to keep a version history
+for TAG in "${BUILD_TAGS[@]}"; do
+  BUILD_TAGS+=("${TAG}-${TIMESTAMP}")
+done
+
+# echo "Generated the following build tags: "
+for TAG in "${BUILD_TAGS[@]}"; do
+  echo "${TAG}"
+done
