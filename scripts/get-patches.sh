9f20cda7a30ae48a737eb00bba21a060c089fd72
Nolan Poe <npgo22@gmail.com>
2023-11-03 15:47:47 -0700

    isolate more patches from invidious-custom

--- scripts/get-patches.sh
+++ scripts/get-patches.sh
@@ -9,7 +9,7 @@ fi
 cp invidious-custom/patches/*.patch patches/
 cp invidious-custom/patches-api/*.patch patches/
 
-if [ "$(ls -A custom-patches)" ]; then
+if [ -d custom-patches ]; then
     cp custom-patches/*.patch patches/
 fi
 
@@ -17,5 +17,7 @@ fi
 # Always remove patches specfic to yewtu.be
 rm patches/003-proxy-csp.patch
 rm patches/004-donate-page.patch
+rm patches/007-range-header.patch
+rm patches/009-restrict-playback.patch
 rm patches/020-use-separate-domain-for-webhook-requests.patch
 rm patches/021-message-registration.patch
